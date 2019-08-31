78d9bcb3db3358688dfa2d89a72d05466d3cd1fa
Joel Knechtli <j.knechtli@valdperformance.com>
2019-08-31 16:41:49 +1000

    Added update user

--- server/api/auth.js
+++ server/api/auth.js
@@ -1,34 +1,35 @@
 
-const users = [
-  {
-    username: 'joel',
-    birthday: '15/09/1998',
-    age: '20',
-    email: 'abc@com.au',
-    password: '123',
-    valid: undefined,
-    role: 15
-  },
-  {
-    username: 'dave',
-    birthday: '15/09/1987',
-    age: '31',
-    email: 'dave@com.au',
-    password: 'farrow',
-    valid: undefined,
-    role: 10
-  },
-  {
-    username: 'nish',
-    birthday: '15/09/1993',
-    age: '25',
-    email: 'nish@com.au',
-    password: 'ant',
-    valid: undefined,
-    role: 5
-  },
-];
+// const users = [
+//   {
+//     username: 'joel',
+//     birthday: '15/09/1998',
+//     age: '20',
+//     email: 'abc@com.au',
+//     password: '123',
+//     valid: undefined,
+//     role: 15
+//   },
+//   {
+//     username: 'dave',
+//     birthday: '15/09/1987',
+//     age: '31',
+//     email: 'dave@com.au',
+//     password: 'farrow',
+//     valid: undefined,
+//     role: 10
+//   },
+//   {
+//     username: 'nish',
+//     birthday: '15/09/1993',
+//     age: '25',
+//     email: 'nish@com.au',
+//     password: 'ant',
+//     valid: undefined,
+//     role: 5
+//   },
+// ];
 
+const users = require('./storage/users.json');
 
 module.exports = (req, res) => {
 
@@ -40,6 +41,8 @@ module.exports = (req, res) => {
   customer.email = req.body.email;
   customer.upwd = req.body.upwd;
   customer.valid = false;
+
+  console.log(users);
 
   users.forEach(user => {
     if (req.body.email === user.email && req.body.upwd === user.upwd) {
---
+++ server/api/updateUser.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+
+module.exports = (req, res) => {
+  const users = require('./../storage/users.json');
+
+  if (!req.body) {
+    return res.sendStatus(400);
+  }
+
+  const email = req.body.email;
+  // customer.upwd = req.body.upwd;
+  // customer.valid = false;
+
+  console.log(users);
+
+  users.forEach(user => {
+    if (email === user.email) {
+      user.age = req.body.age;
+      user.username = req.body.username;
+      user.birthday = req.body.birthday;
+      user.groups = req.body.groups;
+    }
+  });
+
+  const jsonString = JSON.stringify(users);
+
+  fs.writeFile('./../storage/user.json', jsonString, err => {
+    if (err) {
+      console.log('Error writing file: ', err);
+    } else {
+      console.log('Successfully wrote file');
+    }
+  })
+
+}
