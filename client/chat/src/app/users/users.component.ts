33e846fc7ca01bb8dd3152b3615b637b5afe03e0
Joel Knechtli <j.knechtli@valdperformance.com>
2019-09-02 13:47:58 +1000

    Made users display all users better

--- client/chat/src/app/users/users.component.ts
+++ client/chat/src/app/users/users.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { IUser } from '../login/login.component';
+import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const url: string = "http://localhost:3000/api";
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Component({
   selector: 'app-users',
@@ -6,10 +15,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  private users: IUser[] = [];
 
-  constructor() { }
+  constructor(private router: Router, private httpClient: HttpClient) { }
 
   ngOnInit() {
+    this.httpClient.get<IUser[]>(url + '/users', httpOptions)
+      .subscribe((data) => {
+        if (data) {
+          console.log("data: ", data);
+          this.users = data;
+        }
+      });
   }
 
+  editUser(user: IUser) {
+    console.log(user);
+
+    this.router.navigateByUrl(`/users/${user.username}/edit`);
+  }
+
+  getRole(role: number) {
+    switch (role) {
+      case 0: return "Basic User";
+      case 5: return "Group Assistant";
+      case 10: return "Group Admin";
+      case 15: return "Super Admin";
+      default: return "Unknown Role";
+    }
+  }
 }
