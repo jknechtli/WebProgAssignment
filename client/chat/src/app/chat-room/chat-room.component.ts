206018b5f5e6c9a6f72287b68d8ffc2ea1df683b
Joel Knechtli <j.knechtli@valdperformance.com>
2019-09-02 14:41:44 +1000

    mocked up chat room

--- client/chat/src/app/chat-room/chat-room.component.ts
+++ client/chat/src/app/chat-room/chat-room.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-room.component.css']
 })
 export class ChatRoomComponent implements OnInit {
+  private log: string[] = [];
+  private toSend: string;
 
   constructor() { }
 
   ngOnInit() {
+    // get logs
   }
 
+  updateToSend(event) {
+    this.toSend = event.target.value;
+  }
+
+  send() {
+    this.log.push(this.toSend);
+    this.toSend = '';
+  }
 }
