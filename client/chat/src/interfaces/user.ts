56cd44a6b66bda58dd5dd9eae6945f86a5cde637
Joel Knechtli <j.knechtli@valdperformance.com>
2019-09-06 08:47:02 +1000

    Added interface file

--- client/chat/src/interfaces/user.ts
+++ client/chat/src/interfaces/user.ts
@@ -11,6 +11,17 @@ export interface IUser {
   email: string;
   password: string;
   valid: boolean;
-  role: 0 | 5 | 10 | 15; // basic|groupAssis|groupAdmin|superAdmin
+  role: 0 | 5 | 10 | 15; // basic | groupAssis | groupAdmin | superAdmin
   groups: IGroup[];
+}
+
+export interface IChannel {
+  name: string,
+  users: string[]
+}
+
+export interface INewGroup {
+  name: string,
+  channels: IChannel[],
+  users: string[]
 }
