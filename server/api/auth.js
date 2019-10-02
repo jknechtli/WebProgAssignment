fb44b2fed2a8fc887e8445ee38a07a16f3f29a34
Joel Knechtli <j.knechtli@valdperformance.com>
2019-10-03 09:47:06 +1000

    Changed login to use username instead of email

--- server/api/auth.js
+++ server/api/auth.js
@@ -1,9 +1,6 @@
-const fs = require('fs')
-// const users = require('./../storage/users.json');
-
 /**
- * This receives an email and password,
- * and checks if the email and password match a current user.
+ * This receives an username and password,
+ * and checks if the username and password match a current user.
  * If they do, this will return the user with valid == true.
  * If not, this will return the received data with valid == false.
  */
@@ -16,26 +13,19 @@ module.exports = (db, app) => {
     }
 
     const customer = {};
-    customer.email = req.body.email;
+    customer.username = req.body.username;
     customer.password = req.body.password;
 
-    // console.log(product);
     const collection = db.collection('users');
-    //check for duplicate id's
+
     collection.find(customer).limit(1).toArray((err, users) => {
       if (users.length == 0) {
         res.send({ success: 0, topnum: 0 });
       } else {
-        //On send back highest used number.
-        // collection.find({}, { sort: { id: -1 }, limit: 1 }).toArray(function (err, items) {
-        //   console.log(items[0].id);
-        // res.send({ success: 0, topnum: items[0].id });
-        // });
         const user = users[0];
         user.valid = true;
         res.send(user);
 
-        //res.send({'success':0});
       }
     });
 
