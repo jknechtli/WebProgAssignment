6469691e9694cbe29789d07bfe931eba702b8226
Joel Knechtli <j.knechtli@valdperformance.com>
2019-10-03 15:49:04 +1000

    added more tests

--- server/tests/test.js
+++ server/tests/test.js
@@ -1,5 +1,5 @@
 // const expect = require('expect');
-const app = require('../server.js');
+// const app = require('../server.js');
 
 const chai = require('chai');
 // const expect = require('chai').expect;
@@ -26,31 +26,142 @@ describe('Server test', function () {
   });
 
   describe('/api/user/:id', () => {
-    it('it should get users', () => {
-      return sleep(10)
-        .then(() => {
-          chai.request('http://localhost:3001')
-            .get('/api/user/john')
-            .end((err, res) => {
-              res.should.have.status(200);
-              res.body[0].should.have.property('username');
-              res.body[0].should.have.property('role');
-            })
+    it('it should get users', (done) => {
+      chai.request('http://localhost:3001')
+        .get('/api/user/john')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('username');
+          res.body.should.have.property('role');
+          done()
         })
     });
   });
 
   describe('/api/users', () => {
-    it('it should GET all the users', () => {
-      return sleep(10)
-        .then(() => {
-          chai.request('http://localhost:3001')
-            .get('/api/users')
-            .end((err, res) => {
-              res.should.have.status(200);
-              res.body.should.be.an('array');
-            })
+    it('it should GET all the users', (done) => {
+      chai.request('http://localhost:3001')
+        .get('/api/users')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          done()
         })
     });
   });
+
+  describe('/api/groups', () => {
+    it('it should GET all the groups', (done) => {
+      chai.request('http://localhost:3001')
+        .get('/api/groups')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          done()
+        })
+    });
+  });
+
+  describe('/api/auth', () => {
+    it('it should GET authorized user', (done) => {
+      chai.request('http://localhost:3001')
+        .post('/api/auth')
+        .send({ username: 'test', password: 'test' })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('username');
+          done()
+        })
+    });
+  });
+
+  describe('/api/group', () => {
+    it('it should create group', (done) => {
+      chai.request('http://localhost:3001')
+        .post('/api/group')
+        .send({
+          name: 'test',
+          channels: [
+            {
+              name: 'cTest',
+              users: ['test-username']
+            }
+          ]
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('num');
+          done()
+        })
+    });
+  });
+
+  describe('/api/user', () => {
+    it('it should create a user', (done) => {
+      chai.request('http://localhost:3001')
+        .post('/api/user')
+        .send({
+          username: 'test-username',
+          email: 'test-email',
+          role: 'test-role',
+          groups: 'test-groups',
+          birthday: 'test-birthday',
+          password: 'test-password',
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('num');
+          done()
+        })
+    });
+  });
+
+  describe('/api/user/:id', () => {
+    it('it should get a user', (done) => {
+      chai.request('http://localhost:3001')
+        .get('/api/user/test-username')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('username');
+          done()
+        })
+    });
+  });
+
+  describe('/api/user/:id/groups', () => {
+    it('it should get a user', (done) => {
+      chai.request('http://localhost:3001')
+        .get('/api/user/test-username/groups')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          done()
+        })
+    });
+  });
+
+  describe('/api/user/:id/delete', () => {
+    it('it should delete a user', (done) => {
+      chai.request('http://localhost:3001')
+        .delete('/api/user/test-username/delete')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('ok');
+          done()
+        })
+    });
+  });
+
+  describe('/api/chat/:group/:channel', () => {
+    it('it should GET all the chat logs', (done) => {
+      chai.request('http://localhost:3001')
+        .get('/api/chat/based/4chan')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          done()
+        })
+    });
+  });
+
 });
